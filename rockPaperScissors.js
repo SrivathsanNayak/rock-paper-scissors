2070d9be3e49ef297098e534072a4856c2a99d7b
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-07-12 20:23:44 +0530

    Function to count player and computer points added

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,58 +1,71 @@
 let playerScore = 0;
 let computerScore = 0;
 
-const rockButton = document.querySelector("#rock-btn");
-rockButton.addEventListener('click', () => {
-    console.log(playRound('Rock'));
-});
+function game() {
+    const rockButton = document.querySelector("#rock-btn");
+    rockButton.addEventListener('click', () => {
+        console.log(playRound('Rock'));
+        console.log(gameScore());
+    });
 
-const paperButton = document.querySelector("#paper-btn");
-paperButton.addEventListener('click', () => {
-    console.log(playRound('Paper'));
-});
+    const paperButton = document.querySelector("#paper-btn");
+    paperButton.addEventListener('click', () => {
+        console.log(playRound('Paper'));
+        console.log(gameScore());
+    });
 
-const scissorsButton = document.querySelector("#scissors-btn");
-scissorsButton.addEventListener('click', () => {
-    console.log(playRound('Scissors'));
-});
+    const scissorsButton = document.querySelector("#scissors-btn");
+    scissorsButton.addEventListener('click', () => {
+        console.log(playRound('Scissors'));
+        console.log(gameScore());
+    });
 
-/*
-computerPlay() returns computer's randomly generated choice
-choices is array of choices available
-Math.random() gives a random floating-point number between 0 and 1
-It is multiplied by choices.length(that is, 3) to give a number between 0 and 3
-Math.floor() is used to round off the number to the shortest whole number, so we will randomly get a number from 0,1 and 2
-*/
-function computerPlay() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let choice = choices[Math.floor(Math.random() * choices.length)];
-    return choice;
-}
+    const infoPara = document.querySelector("#info");
+    const roundPara = document.querySelector("#round");
+    const finalPara = document.querySelector("#final-result");
 
-/*
-playRound() takes two parameters: playerSelection and computerSelection
-It returns the string which describes the result of the round
-if-else logic is used to compare and determine
-The playerSelection and computerSelection strings are compared by converting both of them to lowercase
-For a win, the player is awarded 1 point; for a loss, the computer is awarded 1 point; for a draw, no points
-*/
-function playRound(playerSelection) {
-    //To refine the input, make the first letter uppercase and the rest lowercase; makes it easier to compare with computerSelection
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
-    const computerSelection = computerPlay();
+    /*
+    computerPlay() returns computer's randomly generated choice
+    choices is array of choices available
+    Math.random() gives a random floating-point number between 0 and 1
+    It is multiplied by choices.length(that is, 3) to give a number between 0 and 3
+    Math.floor() is used to round off the number to the shortest whole number, so we will randomly get a number from 0,1 and 2
+    */
+    function computerPlay() {
+        let choices = ['Rock', 'Paper', 'Scissors'];
+        let choice = choices[Math.floor(Math.random() * choices.length)];
+        return choice;
+    }
 
-    if (playerSelection === computerSelection) {
-        return `It's a draw. ${playerSelection} cancels out ${computerSelection}`;
-    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Paper')
-        || (playerSelection === 'Paper' && computerSelection === 'Rock')) {
-        playerScore++;
-        return `It's a win! ${playerSelection} beats ${computerSelection}`;
-    } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissors')
-        || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-        computerScore++;
-        return `It's a loss! ${playerSelection} loses to ${computerSelection}`;
-    } else {
-        return `Invalid input! Please try again`;
+    /*
+    playRound() takes two parameters: playerSelection and computerSelection
+    It returns the string which describes the result of the round
+    if-else logic is used to compare and determine
+    The playerSelection and computerSelection strings are compared by converting both of them to lowercase
+    For a win, the player is awarded 1 point; for a loss, the computer is awarded 1 point; for a draw, no points
+    */
+    function playRound(playerSelection) {
+        //To refine the input, make the first letter uppercase and the rest lowercase; makes it easier to compare with computerSelection
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
+        const computerSelection = computerPlay();
+
+        if (playerSelection === computerSelection) {
+            return `It's a draw. ${playerSelection} cancels out ${computerSelection}`;
+        } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Paper')
+            || (playerSelection === 'Paper' && computerSelection === 'Rock')) {
+            playerScore++;
+            return `It's a win! ${playerSelection} beats ${computerSelection}`;
+        } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissors')
+            || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+            computerScore++;
+            return `It's a loss! ${playerSelection} loses to ${computerSelection}`;
+        } else {
+            return `Invalid input! Please try again`;
+        }
+    }
+
+    function gameScore() {
+        return (`You: ${playerScore}\nComputer: ${computerScore}`);
     }
 }
 
@@ -60,8 +73,7 @@ function playRound(playerSelection) {
 game() will have a while loop for calling playRound() until either player or computer has 5 points
 When the player or the computer reaches 5 points, alert is shown with the result of the game
 */
-function game() {
-    /*
+/*function game() {
     while (playerScore < 5 && computerScore < 5) {
         let playerSelection = prompt("Enter rock, paper or scissors: ");
         const computerSelection = computerPlay();
@@ -74,7 +86,6 @@ function game() {
     } else {
         alert("You lost! The computer has defeated you");
     }
-    */
 }
-
-//game();
+*/
+game();
